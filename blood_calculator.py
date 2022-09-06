030b6ea48cc376d7275af6c64a5d81a2c2cb9c9d
Bishoy Hanna <bishoy.hanna@duke.edu>
2022-09-05 23:49:35 -0400

    Wrote basic framework for user input

---
+++ blood_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 23:45:15 2022
+
+@author: bishoyzaki
+"""
+
+def interface():
+    print("Blood Calculator")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+   
+interface()
