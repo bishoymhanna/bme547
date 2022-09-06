6b5d5c65c332d13b8dac6dfe965abbb679bb6a94
Bishoy Hanna <bishoy.hanna@duke.edu>
2022-09-06 00:18:11 -0400

    Fixed name for outout_HDL_result function and added HDL option to main interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -9,12 +9,16 @@ Created on Mon Sep  5 23:45:15 2022
 def interface():
     print("Blood Calculator")
     print("Options:")
+    print("1 - Analyze HDL")
     print("9 - Quit")
     keep_running = True
     while keep_running:  
         choice = input("Enter your choice: ")
-        if choice =='9':
+        if choice == "9":
             return
+        elif choice == "1":
+            HDL_driver()
+            
         
 def input_HDL():
     HDL_input = input("Enter the HDL value:")
@@ -31,9 +35,9 @@ def check_HDL(HDL_value):
 def HDL_driver():
     hdl_value = input_HDL()
     answer = check_HDL(hdl_value)
-    output_result(hdl_value, answer)
+    output_HDL_result(hdl_value, answer)
 
-def output_result(hdl_value, charac):
+def output_HDL_result(hdl_value, charac):
     print("The result of an HDL value of {} if {}".format(hdl_value, charac))
    
 interface()
