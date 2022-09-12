bae586144089f6f7d4bb0d38f06e6202e11d4fd8
Bishoy Hanna <bishoy.hanna@duke.edu>
2022-09-12 12:05:21 -0400

    Adding files for 9/12 lecture

---
+++ output_readability.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 12:03:00 2022
+
+@author: bishoyzaki
+"""
+
+
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
