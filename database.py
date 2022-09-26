e266044eb834649bb525c347129ca2c94ac311d6
Bishoy Hanna <bishoy.hanna@duke.edu>
2022-09-26 13:17:33 -0400

    Craeted person class and initlize variables to define the object

--- database.py
+++ database.py
@@ -7,26 +7,40 @@ Created on Wed Sep 14 12:29:30 2022
 """
 
 
+class Patient:
+
+    def __init__(self, first_name, last_name, patient_id, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.patient_id = patient_id
+        self.age = age
+        self.tests = []
+
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
 def create_patient_entry(patient_first_name,
                          patient_last_name,
                          patient_id,
                          patient_age):
-    new_patient = {"First Name": patient_first_name,
-                   "Last Name": patient_last_name,
-                   "Id": patient_id,
-                   "Age": patient_age,
-                   "Tests": []}
+    new_patient = Patient(patient_first_name, patient_last_name,
+                          patient_id, patient_age)
     return new_patient
 
 
 def main():
-    db = {}
-    db[11] = create_patient_entry("Ann", "Ables", 11, 30)
-    db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
-    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
-    print_database(db)
-    add_test_result(db, 3, "HDL", 100)
-    print_database(db)
+    x = Patient("David", "Ward", "", "")
+    print(x.full_name())
+    y = Patient("Edward", "Smith", "", "")
+    print(y.last_name)
+    # db = {}
+    # db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    # db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    # db[3] = create_patient_entry("Chris", "Chou", 3, 25)
+    # print_database(db)
+    # add_test_result(db, 3, "HDL", 100)
+    # print_database(db)
     # print("Patient {} is a {}".format(get_full_name(db[2]),
     #                                  adult_or_minor(db[2])))
     # print(find_patient(db, 3))
