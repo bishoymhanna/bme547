0cff05f536a9ec284bfd7289050fa45a6a1a95a5
Bishoy Hanna <bishoy.hanna@duke.edu>
2022-10-21 17:09:43 -0400

    Web intro class, completed blood check class assignment to see if donor could donate blood to recipient then fixed PEP-8 errors

--- blood_check.py
+++ blood_check.py
@@ -2,9 +2,11 @@ import requests
 
 r = requests.get("http://vcm-7631.vm.duke.edu:5002/get_patients/bmh61")
 print(r.text)
-donor_blood = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/F2")
+donor_blood = requests.get("http://vcm-7631.vm.duke.edu:5002/"
+                           "get_blood_type/F2")
 print(donor_blood.text)
-recipient_blood = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/M6")
+recipient_blood = requests.get("http://vcm-7631.vm.duke.edu:5002/"
+                               "get_blood_type/M6")
 print(recipient_blood.text)
 print("B- cannot take blood from A- so not acceptable blood match")
 match = {"Name": "bmh61", "Match": "No"}
