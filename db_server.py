20eed91d3656cbde3dffd78e3435a93c10e7d832
Bishoy Hanna <bishoy.hanna@duke.edu>
2022-10-26 13:14:41 -0400

    added post request for adding test to patient

--- db_server.py
+++ db_server.py
@@ -76,6 +76,47 @@ def validate_new_patient_info(in_data):
     return True
 
 
+@app.route("/add_test", methods=["POST"])
+def add_test_flask_handler():
+    in_data = request.get_json()
+    msg, status_code = add_test_worker(in_data)
+    return msg, status_code
+
+
+def add_test_worker(in_data):
+    msg = add_test_validation(in_data)
+    if msg is not True:
+        return msg, 400
+    add_test_to_patient(in_data)
+    return "Test added", 200
+
+
+def find_patient(patient_id):
+    for patient in db:
+        if patient["id"] == patient_id:
+            return patient
+    return False
+
+
+def add_test_to_patient(in_data):
+    patient = find_patient(in_data["id"])
+    patient["test_name"].append(in_data["test_name"])
+    patient["test_result"].append(in_data["test_result"])
+
+
+def add_test_validation(in_data):
+    if type(in_data) is not dict:
+        return "Post data was not a dictionary"
+    expected_keys = ["id", "test_name", "test_result"]
+    expected_types = [int, str, int]
+    for ex_key, ex_type in zip(expected_keys, expected_types):
+        if ex_key not in in_data:
+            return "Key missing"
+        if type(in_data[ex_key]) is not ex_type:
+            return "Bad value type"
+    return True
+
+
 if __name__ == '__main__':
     init_server()
     app.run()
